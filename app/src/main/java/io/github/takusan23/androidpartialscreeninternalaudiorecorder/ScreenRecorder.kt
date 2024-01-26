1102c103bcbe6bca7020ca35b7e975328d9a9f42
takusan23 <takumu1200@gmail.com>
2024-01-27 03:30:41 +0900

    OpenGL を使って、単一アプリが表示されていない時は、その旨を表示するように修正。

--- app/src/main/java/io/github/takusan23/androidpartialscreeninternalaudiorecorder/ScreenRecorder.kt
+++ app/src/main/java/io/github/takusan23/androidpartialscreeninternalaudiorecorder/ScreenRecorder.kt
@@ -2,17 +2,28 @@ package io.github.takusan23.androidpartialscreeninternalaudiorecorder
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.hardware.display.DisplayManager
 import android.hardware.display.VirtualDisplay
 import android.media.MediaRecorder
 import android.media.projection.MediaProjection
 import android.media.projection.MediaProjectionManager
 import android.os.Build
+import io.github.takusan23.androidpartialscreeninternalaudiorecorder.opengl.InputSurface
+import io.github.takusan23.androidpartialscreeninternalaudiorecorder.opengl.TextureRenderer
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.withContext
 import java.io.File
 
@@ -31,6 +42,8 @@ class ScreenRecorder(
     private var videoRecordingFile: File? = null
     private var virtualDisplay: VirtualDisplay? = null
     private var internalAudioRecorder: InternalAudioRecorder? = null
+    private var inputOpenGlSurface: InputSurface? = null
+    private var isDrawAltImage = false
 
     /** 録画を開始する */
     fun startRecord() {
@@ -65,6 +78,7 @@ class ScreenRecorder(
                     override fun onCapturedContentVisibilityChanged(isVisible: Boolean) {
                         super.onCapturedContentVisibilityChanged(isVisible)
                         // 録画中の画面の表示・非表示が切り替わったら呼び出される
+                        isDrawAltImage = !isVisible
                     }
 
                     override fun onStop() {
@@ -74,6 +88,15 @@ class ScreenRecorder(
                     }
                 }, null)
             }
+            // OpenGL を経由して、画面共有の映像を MediaRecorder へ渡す
+            // スレッド注意
+            withContext(openGlRelatedDispatcher) {
+                inputOpenGlSurface = InputSurface(mediaRecorder?.surface!!, TextureRenderer())
+                inputOpenGlSurface?.makeCurrent()
+                inputOpenGlSurface?.createRender(VIDEO_WIDTH, VIDEO_HEIGHT)
+                // 単一アプリが画面に写っていないときに描画する、代替画像をセット
+                inputOpenGlSurface?.setAltImageTexture(createAltImage())
+            }
             // 画面ミラーリング
             virtualDisplay = mediaProjection?.createVirtualDisplay(
                 "io.github.takusan23.androidpartialscreeninternalaudiorecorder",
@@ -81,7 +104,7 @@ class ScreenRecorder(
                 VIDEO_HEIGHT,
                 context.resources.configuration.densityDpi,
                 DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-                mediaRecorder?.surface,
+                inputOpenGlSurface?.drawSurface,
                 null,
                 null
             )
@@ -94,11 +117,39 @@ class ScreenRecorder(
 
             // 画面録画開始
             mediaRecorder?.start()
-            // 内部音声収録開始。
-            // この関数のあとに処理を書いても、startRecord が一時停止し続けるので注意。
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                internalAudioRecorder?.startRecord()
-            }
+            // OpenGL と 内部音声の処理を始める
+            // 並列で
+            listOf(
+                launch(openGlRelatedDispatcher) {
+                    // OpenGL で画面共有か代替画像のどちらかを描画する
+                    while (isActive) {
+                        try {
+                            if (isDrawAltImage) {
+                                inputOpenGlSurface?.drawAltImage()
+                                inputOpenGlSurface?.swapBuffers()
+                                delay(16) // 60fps が 16ミリ秒 らしいので適当に待つ。多分待たないといけない
+                            } else {
+                                // 映像フレームが来ていれば OpenGL のテクスチャを更新
+                                val isNewFrameAvailable = inputOpenGlSurface?.awaitIsNewFrameAvailable()
+                                // 描画する
+                                if (isNewFrameAvailable == true) {
+                                    inputOpenGlSurface?.updateTexImage()
+                                    inputOpenGlSurface?.drawImage()
+                                    inputOpenGlSurface?.swapBuffers()
+                                }
+                            }
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
+                },
+                launch {
+                    // 内部音声収録開始。
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        internalAudioRecorder?.startRecord()
+                    }
+                }
+            ).joinAll() // 終わるまで一時停止
         }
     }
 
@@ -140,7 +191,31 @@ class ScreenRecorder(
         }
     }
 
+    /** 単一アプリの画面録画時に、指定した単一アプリが画面外に移動した際に代わりに描画する画像を生成する */
+    private fun createAltImage(): Bitmap = Bitmap.createBitmap(VIDEO_WIDTH, VIDEO_HEIGHT, Bitmap.Config.ARGB_8888).also { bitmap ->
+        val canvas = Canvas(bitmap)
+        val paint = Paint().apply {
+            color = Color.WHITE
+            textSize = 50f
+        }
+
+        canvas.drawColor(Color.BLACK)
+        canvas.drawText("指定したアプリは今画面に写ってません。", 100f, 100f, paint)
+        canvas.drawText("戻ってきたら映像が再開されます。", 100f, 200f, paint)
+    }
+
     companion object {
+
+        /**
+         * OpenGL はスレッドでコンテキストを識別するので、OpenGL 関連はこの openGlRelatedDispatcher から呼び出す。
+         * どういうことかと言うと、OpenGL は makeCurrent したスレッド以外で、OpenGL の関数を呼び出してはいけない。
+         * （makeCurrent したスレッドのみ swapBuffers 等できる）。
+         *
+         * 独自 Dispatcher を作ることで、処理するスレッドを指定できたりする。
+         */
+        @OptIn(DelicateCoroutinesApi::class)
+        private val openGlRelatedDispatcher = newSingleThreadContext("OpenGLContextRelatedThread")
+
         private const val VIDEO_WIDTH = 1280
         private const val VIDEO_HEIGHT = 720
         private const val AUDIO_SAMPLING_RATE = 44_100
